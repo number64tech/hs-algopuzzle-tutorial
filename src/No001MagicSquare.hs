fb49bb70394e71fc698e6710f91e65688259f5ea
TODOROKI Daisuke <number64tech@gmail.com>
2016-02-19 16:23:24 +0900

    [WIP]

--- src/No001MagicSquare.hs
+++ src/No001MagicSquare.hs
@@ -1,5 +1,18 @@
 module No001MagicSquare where
 
-calc :: (Int, Int, Int, Int, Int, Int, Int, Int, Int) -> Bool
-calc (a, b, c, d, e, f, g, h, i) = (a + d + g) == (b + e + h)
+square :: [Int]
+square = []
+
+type ToNext = [Int] -> [Int]
+
+appender :: Int -> ToNext
+appender value = \base -> base ++ [value]
+
+toNext = appender 1
+
+check :: [Int] -> [Int]
+check base = toNext base
+
+main :: IO ()
+main = putStrLn (show (check square))
 
