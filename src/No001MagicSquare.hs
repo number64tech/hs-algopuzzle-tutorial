7ca14cdf0954ab9d17ae66b9d4ba98e812bdc3bb
TODOROKI Daisuke <number64tech@gmail.com>
2016-02-20 21:35:41 +0900

    Test commiting

--- src/No001MagicSquare.hs
+++ src/No001MagicSquare.hs
@@ -3,3 +3,5 @@ module No001MagicSquare where
 calc :: (Int, Int, Int, Int, Int, Int, Int, Int, Int) -> Bool
 calc (a, b, c, d, e, f, g, h, i) = (a + d + g) == (b + e + h)
 
+main :: IO()
+main = putStrLn (show "atom test test")
