25b600f80bd9aac5179aea94cfea2a1ba9acc2b0
TODOROKI Daisuke <number64tech@gmail.com>
2016-02-21 21:24:11 +0900

    Commit with Atom

--- src/No001MagicSquare.hs
+++ src/No001MagicSquare.hs
@@ -3,7 +3,5 @@ module No001MagicSquare where
 calc :: (Int, Int, Int, Int, Int, Int, Int, Int, Int) -> Bool
 calc (a, b, c, d, e, f, g, h, i) = (a + d + g) == (b + e + h)
 
-diffline
-
 main :: IO()
 main = putStrLn (show "atom test test")
