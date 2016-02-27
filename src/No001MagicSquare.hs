202f4aff3b70386a3b02ea9d649d67c6f2b8cf42
TODOROKI Daisuke <number64tech@gmail.com>
2016-02-27 21:56:03 +0900

    [WIP]

--- src/No001MagicSquare.hs
+++ src/No001MagicSquare.hs
@@ -1,1 +1,14 @@
 module No001MagicSquare where
+
+--
+checkEstablished :: [Int] -> Maybe Boolean
+checkEstablished square let size = (length square)
+  | (size < 5) = Nothing
+  | (size = 6) = false
+  | otherwise = true
+
+isEstablished :: Boolean
+isEstablished = checkEstablished [1,2,3,4,5,6,]
+
+main :: IO ()
+main = putStrLn (show isEstablished)
