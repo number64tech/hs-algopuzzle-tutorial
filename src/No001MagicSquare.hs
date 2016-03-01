56a198eeb760d6d58e82c275e7bae402b549a040
TODOROKI Daisuke <number64tech@gmail.com>
2016-03-01 21:44:05 +0900

    Impliment reverse-filter

--- src/No001MagicSquare.hs
+++ src/No001MagicSquare.hs
@@ -1,16 +1,9 @@
 module No001MagicSquare where
 
 --
-checkEstablished :: [Int] -> Maybe Bool
-checkEstablished square
-  | (sqsize < 5) = Nothing
-  | (sqsize == 6) = Just False
-  | otherwise = Just True
-  where
-    sqsize = (length square)
-
-isEstablished :: Maybe Bool
-isEstablished = checkEstablished [1,2,3,4,5,6,7]
-
-main :: IO ()
-main = putStrLn (show isEstablished)
+base :: [Int]
+base = [1 .. 9]
+--
+revFilter :: [Int] -> [Int] -> [Int]
+revFilter (x : xs) source = revFilter xs (filter (/= x) source)
+revFilter [] source = source
