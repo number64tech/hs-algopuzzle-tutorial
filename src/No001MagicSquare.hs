be24c7e9ac3bfa52dafd5a7f96bedb9bd4d98da4
TODOROKI Daisuke <number64tech@gmail.com>
2016-03-02 01:10:36 +0900

    [WIP]

--- src/No001MagicSquare.hs
+++ src/No001MagicSquare.hs
@@ -7,3 +7,13 @@ base = [1 .. 9]
 revFilter :: [Int] -> [Int] -> [Int]
 revFilter (x : xs) source = revFilter xs (filter (/= x) source)
 revFilter [] source = source
+-- to generate brother node(s), or child(ren)
+noder :: Int -> [Int] -> [Int] -> Int
+noder count (child : otherChildren) parentSqr
+  | squareSize == 9 = 1 + count
+  | otherChildren == [] = noder count candidates square
+  | otherwise = noder (noder count otherChildren parentSqr) candidates square
+  where
+    square = parentSqr ++ [child]
+    squareSize = length (square)
+    candidates = revFilter parentSqr base
