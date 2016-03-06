d235ee12f1b0870e3bc41302b912ba16a2dd3f7e
TODOROKI Daisuke <number64tech@gmail.com>
2016-03-07 03:44:05 +0900

    [WIP] debugging

--- src/No001MagicSquare.hs
+++ src/No001MagicSquare.hs
@@ -10,10 +10,14 @@ revFilter [] source = source
 --
 isCorresponding :: [Int] -> Int
 isCorresponding square
+  | (sq22 /= 5) = 0
   | (sq12 + sq13) /= (sq21 + sq31) = 0
-  | (sq13 + sq23) /= (sq31 + sq32) = 0
-  | (sq11 + sq12) /= (sq23 + sq33) = 0
-  | ((sq11 + sq12 + sq13)  == (sq11 + sq22 + sq33)) && ((sq11 + sq33) == (sq13 + sq31)) = 1
+  | (sq11 + sq12 + sq13) == (sq21 + sq22 + sq23) &&
+    (sq11 + sq12 + sq13) == (sq31 + sq32 + sq33) &&
+    (sq11 + sq12 + sq13) == (sq12 + sq22 + sq32) &&
+    (sq11 + sq12 + sq13) == (sq13 + sq23 + sq33) &&
+    (sq11 + sq12 + sq13) == (sq11 + sq22 + sq33) &&
+    (sq11 + sq12 + sq13) == (sq13 + sq22 + sq31) = 1
   | otherwise = 0
   where
     sq11 = square !! 0
@@ -26,11 +30,11 @@ isCorresponding square
     sq32 = square !! 7
     sq33 = square !! 8
 -- to generate brother node(s), or child(ren)
-noder :: Int -> [Int] -> [Int] -> Int
-noder count (child : otherChildren) parentSqr
+derivation :: Int -> [Int] -> [Int] -> Int
+derivation count (child : otherChildren) parentSqr
   | squareSize == 9 = (isCorresponding square) + count
-  | otherChildren == [] = noder count candidates square
-  | otherwise = noder (noder count otherChildren parentSqr) candidates square
+  | otherChildren == [] = derivation count candidates square
+  | otherwise = derivation (derivation count otherChildren parentSqr) candidates square
   where
     square = parentSqr ++ [child]
     squareSize = length (square)
