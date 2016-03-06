ed889e045d1f1ee03312e44158e95ab7052f3072
TODOROKI Daisuke <number64tech@gmail.com>
2016-03-06 11:35:06 +0900

    [WIP]

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,27 @@
-module Main where
-
 import Debug.Trace
 import No001MagicSquare
 
 main :: IO ()
-main = traceIO ""
+
+-- No001MagicSquare
+-- division count children parent
+
+-- A correct pattern
+--main = putStrLn (show (derivation 0 [2] [8, 3, 4, 1, 5, 9, 6, 7]))
+
+-- 0
+--main = putStrLn (show (derivation 0 [1, 3, 4, 5, 6, 7, 8, 9] [2]))
+-- 1
+--main = putStrLn (show (derivation 0 [1, 2, 3, 5, 6, 7, 8, 9] [4]))
+-- 1
+--main = putStrLn (show (derivation 0 [1, 2, 3, 4, 5, 7, 8, 9] [6]))
+-- 2
+main = putStrLn (show (derivation 0 [1, 2, 3, 4, 5, 6, 7, 9] [8]))
+
+
+
+
+--
+
+
+-- No002NQueen
