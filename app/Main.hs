2892e55d914a259df5563974ce5da76f765425fe
TODOROKI Daisuke <number64tech@gmail.com>
2016-03-09 23:47:47 +0900

    Implement No001 MagicSquare !!!!

--- app/Main.hs
+++ app/Main.hs
@@ -4,24 +4,7 @@ import No001MagicSquare
 main :: IO ()
 
 -- No001MagicSquare
--- division count children parent
-
--- A correct pattern
 --main = putStrLn (show (derivation 0 [2] [8, 3, 4, 1, 5, 9, 6, 7]))
-
--- 0
---main = putStrLn (show (derivation 0 [1, 3, 4, 5, 6, 7, 8, 9] [2]))
--- 1
---main = putStrLn (show (derivation 0 [1, 2, 3, 5, 6, 7, 8, 9] [4]))
--- 1
---main = putStrLn (show (derivation 0 [1, 2, 3, 4, 5, 7, 8, 9] [6]))
--- 2
-main = putStrLn (show (derivation 0 [1, 2, 3, 4, 5, 6, 7, 9] [8]))
-
-
-
-
---
-
+main = putStrLn (show (derivation 0 [1 .. 9] []))
 
 -- No002NQueen
--- src/No001MagicSquare.hs
+++ src/No001MagicSquare.hs
@@ -5,19 +5,19 @@ base :: [Int]
 base = [1 .. 9]
 --
 revFilter :: [Int] -> [Int] -> [Int]
-revFilter (x : xs) source = revFilter xs (filter (/= x) source)
 revFilter [] source = source
+revFilter (x : xs) source = revFilter xs (filter (/= x) source)
 --
 isCorresponding :: [Int] -> Int
 isCorresponding square
   | (sq22 /= 5) = 0
   | (sq12 + sq13) /= (sq21 + sq31) = 0
-  | (sq11 + sq12 + sq13) == (sq21 + sq22 + sq23) &&
-    (sq11 + sq12 + sq13) == (sq31 + sq32 + sq33) &&
-    (sq11 + sq12 + sq13) == (sq12 + sq22 + sq32) &&
-    (sq11 + sq12 + sq13) == (sq13 + sq23 + sq33) &&
-    (sq11 + sq12 + sq13) == (sq11 + sq22 + sq33) &&
-    (sq11 + sq12 + sq13) == (sq13 + sq22 + sq31) = 1
+  | (((sq11 + sq12 + sq13) == (sq21 + sq22 + sq23)) &&
+    ((sq11 + sq12 + sq13) == (sq31 + sq32 + sq33)) &&
+    ((sq11 + sq12 + sq13) == (sq13 + sq23 + sq33)) &&
+    ((sq11 + sq12 + sq13) == (sq12 + sq22 + sq32)) &&
+    ((sq11 + sq12 + sq13) == (sq11 + sq22 + sq33)) &&
+    ((sq11 + sq12 + sq13) == (sq13 + sq22 + sq31))) = 1
   | otherwise = 0
   where
     sq11 = square !! 0
@@ -38,4 +38,4 @@ derivation count (child : otherChildren) parentSqr
   where
     square = parentSqr ++ [child]
     squareSize = length (square)
-    candidates = revFilter parentSqr base
+    candidates = revFilter square base
